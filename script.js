8f6f9e10d6c161b838e0b5de2703ba5f31e9abc3
Erik <erik.brodin@skola.taby.se>
2024-04-26 15:12:26 +0200

    :(

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let apiKey = "pub_40262c128bb3476afb761921e8f4c9224c6a5"
-let searchString = "pizza"
+let searchString = "sweden"
 var url = `https://newsdata.io/api/1/news?apikey=${apiKey}&q=${searchString}`;
+let newsContainer = document.getElementById("NewsArticles-StyleScrollHorizontal");
 
 async function fetchData() {
     let data = await fetch(url);
@@ -11,4 +12,18 @@ async function fetchData() {
 window.onload = async function() {
     const data = await fetchData();
     console.log(data);
+    displayData(data);
+}
+
+function displayData(data) {
+    let index = 0;
+    console.log(newsContainer);
+    for (let i = 0; i < data.results.length; i++) {
+        let articleDiv = document.createElement("div");
+        articleDiv.innerHTML = data.results[index].title;
+        articleDiv.classList.add("News-Article-StyleScroll");
+        newsContainer.appendChild(articleDiv);
+        index++;
+    }
+
 }
