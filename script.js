683c5911d2c0c59b0b8dd84d75391be47ddf3a01
Erik <erik.brodin@skola.taby.se>
2024-05-13 13:15:44 +0200

    Tog bort javascript och kommer fokusera på css och html. Behöver nu jobba mycket :)

--- script.js
+++ script.js
@@ -1,48 +1,54 @@
-let apiKey = "pub_40262c128bb3476afb761921e8f4c9224c6a5"
+// let apiKey = "5addb6294bc045f6aefdfc95ac2af42c";
+// containerColoumn = document.getElementById("Container-Coloumn");
 
-containerColoumn = document.getElementById("Container-Coloumn");
+// console.log(containerColoumn);
 
+// async function fetchData(searchString) {
+//     let url = `https://newsapi.org/v2/everything?apikey=${apiKey}&q=${searchString}`;
+//     let data = await fetch(url);
+//     let json = await data.json();
+//     return json;
+// }
+// // när fönstret laddas
+// window.onload = async function() {
+//     let foodNews = await fetchData("food");
+//     console.log(foodNews);
+//     displayFoodNews(foodNews);
+//     const sportNews = await fetchData("sport");
+//     displaySportNews(sportNews)
+// }
 
-let funNews = document.getElementById("fun-news");
+// function displayFoodNews(data) {
+//     let newsContainer = document.getElementById("NewsArticles-StyleScrollHorizontal");
+//     let index = 0;
+//     console.log(newsContainer);
+//     for (let i = 0; i < data.results.length; i++) {
+//         let articleDiv = document.createElement("div");
+//         articleDiv.innerHTML = data.results[index].title;
+//         articleDiv.classList.add("News-Article-Scroll");
+//         newsContainer.appendChild(articleDiv);
+//         index++;
+//     }
 
-console.log(containerColoumn);
+// }
 
-async function fetchData(searchString) {
-    let url = `https://newsdata.io/api/1/news?apikey=${apiKey}&q=${searchString}`;
-    let data = await fetch(url);
-    let json = await data.json();
-    return json;
-}
-// när fönstret laddas
-window.onload = async function() {
-    let foodNews = await fetchData("food");
-    console.log(foodNews);
-    displayFoodNews(foodNews);
-    const sportNews = await fetchData("sport");
-    displaySportNews(sportNews)
-}
-
-function displayFoodNews(data) {
-    let newsContainer = document.getElementById("NewsArticles-StyleScrollHorizontal");
-    let index = 0;
-    console.log(newsContainer);
-    for (let i = 0; i < data.results.length; i++) {
-        let articleDiv = document.createElement("div");
-        articleDiv.innerHTML = data.results[index].title;
-        articleDiv.classList.add("News-Article-Scroll");
-        newsContainer.appendChild(articleDiv);
-        index++;
-    }
-
-}
-
-function displaySportNews(data) {
-    let sportNews = document.getElementById("sport-news");
-    let index = 0;
-    for (let i = 0; i < data.results.length; i++) {
-        let articleDiv = document.createElement("div");
-        articleDiv.innerHTML = data.results[index].title;
-        sportNews.appendChild(articleDiv);
-        index++;
-    }
-}
+// function displaySportNews(data) {
+//     let sportNews = document.getElementById("sport-news");
+//     let index = 0;
+//     for (let i = 0; i < data.results.length; i++) {
+//         let articleDiv = document.createElement("div");
+//         articleDiv.innerHTML = data.results[index].title;
+//         sportNews.appendChild(articleDiv);
+//         index++;
+//     }
+// }
+// function displayFunNews(data) {
+//     let funNews = document.getElementById("fun-news");
+//     let index = 0;
+//     for (let i = 0; i < data.results.length; i++) {
+//         let articleDiv = document.createElement("div");
+//         articleDiv.innerHTML = data.results[index].title;
+//         funNews.appendChild(articleDiv);
+//         index++;
+//     }
+// }
